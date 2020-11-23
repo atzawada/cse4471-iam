6f21b469ed8d8543ddefcbe6a4b2e5ebd13aea82
zawada1 <zawada1@nationwide.com>
2020-11-23 10:37:50 -0500

    Add app skeleton

--- src/main/java/edu/osu/cse4471/iam/IamApplication.java
+++ src/main/java/edu/osu/cse4471/iam/IamApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class IamToolApplication {
+public class IamApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(IamToolApplication.class, args);
