121d3f89a97bacd725b42fc7b0114fe29ab5a9a9
zawada1 <zawada1@nationwide.com>
2020-11-23 10:09:08 -0500

    init

---
+++ src/main/java/edu/osu/cse4471/iamtool/IamToolApplication.java
@@ -0,0 +1,13 @@
+package edu.osu.cse4471.iamtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IamToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IamToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/osu/cse4471/iamtool/IamToolApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.osu.cse4471.iamtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IamToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
