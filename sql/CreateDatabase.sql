c2c69a41405beb96ae4b5378ded4565f3006c879
zawada1 <zawada1@nationwide.com>
2020-11-23 22:58:45 -0500

    update userdao

--- sql/CreateDatabase.sql
+++ sql/CreateDatabase.sql
@@ -55,7 +55,73 @@ CREATE TABLE IF NOT EXISTS RULES (
     FOREIGN KEY (`role`)
     REFERENCES `iam`.`role` (`name`)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION)-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
+USE `mydb` ;
+
+-- -----------------------------------------------------
+-- Table `mydb`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`user` (
+  `username` VARCHAR(16) NOT NULL,
+  `email` VARCHAR(255) NULL,
+  `password` VARCHAR(32) NOT NULL,
+  PRIMARY KEY (`username`));
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`role`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`role` (
+  `name` VARCHAR(45) NOT NULL,
+  `description` VARCHAR(150) NULL,
+  `owner` VARCHAR(45) NULL,
+  PRIMARY KEY (`name`),
+  INDEX `user_idx` (`owner` ASC) VISIBLE,
+  CONSTRAINT `user`
+    FOREIGN KEY (`owner`)
+    REFERENCES `mydb`.`user` (`username`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `mydb`.`rules`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `mydb`.`rules` (
+  `user` VARCHAR(45) NULL,
+  `role` VARCHAR(45) NULL,
+  INDEX `username_idx` (`user` ASC) VISIBLE,
+  INDEX `role_idx` (`role` ASC) VISIBLE,
+  CONSTRAINT `username`
+    FOREIGN KEY (`user`)
+    REFERENCES `mydb`.`user` (`username`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `rolename`
+    FOREIGN KEY (`role`)
+    REFERENCES `mydb`.`role` (`name`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 ENGINE = InnoDB;
 
 
--- src/main/java/edu/osu/cse4471/iam/dao/UserDao.java
+++ src/main/java/edu/osu/cse4471/iam/dao/UserDao.java
@@ -1,5 +1,30 @@
 package edu.osu.cse4471.iam.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import edu.osu.cse4471.iam.model.User;
+
+@Component
 public class UserDao {
-    
+    private final JdbcTemplate jdbcTemplate;
+
+    public UserDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void createUser(String username, String password, String email) {
+        this.jdbcTemplate.update("INSERT INTO mydb.user VALUES(:username, :email, :password)", username, email, password);
+    }
+
+    public User getUser(String username) {
+        this.jdbcTemplate.queryForObject("SELECT * FROM mydb.user WHERE USERNAME = :username", (rs, rownum) -> {
+            return new User();
+        }, username);
+        return null;
+    } 
+
+    public void deleteUser() {
+        
+    }
 }
