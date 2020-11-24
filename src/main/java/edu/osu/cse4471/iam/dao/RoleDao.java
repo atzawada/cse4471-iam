3a3cfe45d983175519c757715c5a3d11905306f7
rlsanders4 <59902221+rlsanders4@users.noreply.github.com>
2020-11-23 23:02:15 -0500

    Updated roledao

--- src/main/java/edu/osu/cse4471/iam/dao/RoleDao.java
+++ src/main/java/edu/osu/cse4471/iam/dao/RoleDao.java
@@ -1,8 +1,19 @@
 package edu.osu.cse4471.iam.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RoleDao {
+    private final  JdbcTemplate jdbcTemplate;
+
+    public RoleDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void createRole(String name, String description, String owner) {
+        this.jdbcTemplate.update("INSERT INTO mydb.role VALUES(:name, :description, :owner)", name, description, owner);
+    }
+
     
 }
