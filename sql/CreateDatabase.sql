2771361d96279c75aa1691335e5324c091d4affa
rlsanders4 <59902221+rlsanders4@users.noreply.github.com>
2020-11-23 22:40:27 -0500

    Update models

--- sql/CreateDatabase.sql
+++ sql/CreateDatabase.sql
@@ -21,7 +21,6 @@ CREATE TABLE IF NOT EXISTS `mydb`.`user` (
   `username` VARCHAR(16) NOT NULL,
   `email` VARCHAR(255) NULL,
   `password` VARCHAR(32) NOT NULL,
-  `create_time` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`username`));
 
 
--- src/main/java/edu/osu/cse4471/iam/model/Role.java
+++ src/main/java/edu/osu/cse4471/iam/model/Role.java
@@ -2,6 +2,9 @@ package edu.osu.cse4471.iam.model;
 
 import java.util.List;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
 import lombok.Data;
@@ -9,8 +12,16 @@ import lombok.Data;
 @Data
 @Table("ROLE")
 public class Role {
+    @Id
+    @Column("name")
     private String name;
+
+    @Column("description")
     private String description;
-    private User admin;
+
+    @Column("owner")
+    private String admin;
+
+    @Query()
     private List<User> members;
 }
